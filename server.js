99cd8caffc82a89d1c6cdb4b44ed103c49ad6029
Jvpdo <sxacasd@gmail.com>
2025-02-22 14:43:39 -0300

    first commit

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const db = require('./database/database');
 const app = express();
 const port = 3000;
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose()
+
+//Rota para o banco de dados
+const db = new sqlite3.Database('./database/database.db', (err) =>
+{
+if (err) {
+    console.error('Erro ao abrir o banco de dados:', err.message);
+} else {
+    console.log('Conectado ao banco de dados SQLite.');
+}
+});
 
 
 // Configura o Express para servir arquivos estáticos
