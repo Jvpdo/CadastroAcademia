400df6893df9c0f462faa59e7b97c5f98b23362a
Jvpdo <sxacasd@gmail.com>
2025-08-03 11:38:15 -0300

    Ajusta o limite de conexões do pool para 3

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const pool = mariadb.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  connectionLimit: 5,
+  connectionLimit: 3,
 });
 
 // --- Configuração do Cloudinary ---
